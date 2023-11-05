1a302f71172a7789b24bf8bcc9f3427fca92684f
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-11-05 12:07:47 +0530

    landing page created

--- prayagraj-xports/src/App.js
+++ prayagraj-xports/src/App.js
@@ -1,17 +1,19 @@
 import "locomotive-scroll/dist/locomotive-scroll.css";
+
 import { AnimatePresence } from "framer-motion";
 import { useRef, useState, useEffect } from "react";
 import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import { dark, light } from "./styles/Themes";
+
 import Loader from "./components/Loader";
-import Home from "./sections/Home";
 import ScrollTriggerProxy from "./components/ScrollTriggerProxy";
 import About from "./sections/About";
+import Footer from "./sections/Footer";
+import Home from "./sections/Home";
 import Marquee from "./sections/Marquee";
 import NewArrival from "./sections/NewArrival";
-import Footer from "./sections/Footer";
+ import GlobalStyles from "./styles/GlobalStyles";
+import { dark, light } from "./styles/Themes";
 
 function App() {
   // useLocoScroll();
@@ -26,8 +28,8 @@ function App() {
 
   return (
     <>
-     <GlobalStyles />
-     <ThemeProvider theme={dark}>
+      <GlobalStyles />
+      <ThemeProvider theme={dark}>
         <LocomotiveScrollProvider
           options={{
             smooth: true,
@@ -50,9 +52,10 @@ function App() {
         >
           <AnimatePresence>{Loaded ? null : <Loader />}</AnimatePresence>
           <main className="App" data-scroll-container ref={containerRef}>
-          <ScrollTriggerProxy />
+            <ScrollTriggerProxy />
             <AnimatePresence>
               {Loaded ? null : <Loader />}
+
               <Home key="home" />
               <About key="about" />
               <Marquee key="marquee" />
